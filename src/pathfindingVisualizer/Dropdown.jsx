a316dd8ddd12d9897a887ba0953e4bf494cfde35
Ankit Kumar <mail.ankitkumar14@gmail.com>
2022-11-26 12:59:16 +0530

    completed  navbar development

--- src/pathfindingVisualizer/Dropdown.jsx
+++ src/pathfindingVisualizer/Dropdown.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
+import React from 'react';
+import './Dropdown.css';
+import {useState} from 'react';
 
-function Dropdown() {
+function Dropdown({title,list}) {
+  const [open,setOpen]=useState(false);
+
+  const handleOpen=()=>{
+    setOpen(!open);
+  };
+  // handleClick(menuItems){
+  //   this.setState({open:false});
+  // }
+
   return (
-    <div>Dropdown</div>
+    <div className="dropdown">
+      <button className="dropdown-button" onClick={handleOpen}>{title}<span> &#9662;</span></button>
+      {open?(
+        <ul className="menu"> 
+          {list.map((menuItems,index)=>(
+            <li key={index} className="menu-items">
+              <button onClick={()=>this.handleClick(menuItems)}>{menuItems}</button>
+            </li>
+          ))} 
+        </ul>
+      ):null}
+    </div>
+    
   )
 }
 
--- src/pathfindingVisualizer/Navbar.jsx
+++ src/pathfindingVisualizer/Navbar.jsx
@@ -3,17 +3,20 @@ import Dropdown from './Dropdown';
 import './Navbar.css';
 
 function Navbar() {
+  const algos=['Dijkstras Algorithm','A*Seacrh','Dfs'];
+  const mazes=['maze1','maze2','maze3','maze4'];
+  const speed=['fast','medium','slow'];
   return (
     <div className="navbar">
         <h3>Pathfinding Visualizer</h3>
-        <Dropdown>Algorithms</Dropdown>
-        <Dropdown>Maze & Patterns</Dropdown>
+        <Dropdown title={'Algorithms'} list={algos}/>
+        <Dropdown title={'Maze&Patterns'} list={mazes}/>
         <button className='nav-buttons'>Add bomb</button>
         <button className='nav-buttons extra'>Visualize!</button>
         <button className='nav-buttons'>Clear Path</button>
         <button className='nav-buttons'>Clear Walls & Weights</button>
         <button className='nav-buttons'>Clear Board</button>
-        <Dropdown>Speed</Dropdown>
+        <Dropdown title={'Speed'} list={speed}/>
     </div>
   )
 }
